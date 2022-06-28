d67b3f3e3af5c621c6316ac0ac962e83b792a354
Kai Hwang <kai.hwang@gmail.com>
2022-06-28 15:50:42 -0500

    for quantum.

--- optimize.sh
+++ optimize.sh
@@ -16,3 +16,12 @@ for i in $(seq 1 1 10); do
 	cat search-001.par | grep NULL | awk '{print $3}' > ThalHiITIs/ITI${i}.txt
 
 done
+
+
+# for quantum, 35 compound trials (70), 210 TRs
+for i in $(seq 1 1 10); do 
+
+	optseq2 --ntp 210 --tr 2 --psdwin 0 18 1 --ev cue 1.5 80 --tnullmin 1.5 --tnullmax 10 --nsearch 10000 --ar1 rho --nkeep 1 --o search
+	cat search-001.par | grep NULL | awk '{print $3}' > QuantITIs/ITI${i}.txt
+
+done
